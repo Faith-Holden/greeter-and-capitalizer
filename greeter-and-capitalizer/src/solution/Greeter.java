30aa87d324cb675bc427e73e756ba7bfb811d51b
Faith-Holden <64601950+exhanaver@users.noreply.github.com>
2021-09-03 12:55:35 -0500

    update package name

---
+++ greeter-and-capitalizer/src/solution/Greeter.java
@@ -0,0 +1,11 @@
+package solution;
+
+import textio.TextIO;
+
+public class Greeter {
+    public static void main(String[] args){
+        System.out.println("What is your name?");
+        String name = TextIO.getlnString();
+        System.out.println("Hello "+ name.toUpperCase()+", nice to meet you!");
+    }
+}
